3786747fc7ecafbcee5ff4ab11f1afbe7a56eb90
jaroslawmichalik <michalik@protonmail.ch>
2019-10-20 11:50:38 +0200

    Add endpoint with predefined value

---
+++ src/main/kotlin/tech/michalik/playground/Server.kt
@@ -0,0 +1,16 @@
+package tech.michalik.playground
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun Application.main() {
+    routing {
+        get("/") {
+            call.respond(HttpStatusCode.OK, "Test message\n")
+        }
+    }
+}
